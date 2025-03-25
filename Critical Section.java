a8dae2c4c5f17a74a69ead060b3d30047615485f
ArsamHashmi <41957285+ArsamHashmi@users.noreply.github.com>
2025-03-25 19:02:33 -0400

    Create Critical Section.java

---
+++ Critical Section.java
@@ -0,0 +1,39 @@
+public class CriticalSectionExample {
+
+    static class SharedCounter {
+        private int count = 0;
+
+        public synchronized void increment() {
+            // Critical Section
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SharedCounter counter = new SharedCounter();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final count: " + counter.getCount());
+    }
+}
